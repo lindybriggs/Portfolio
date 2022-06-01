376ba893b6a7bcefc490d95b271fe892d0572744
Lindy Briggs <briggs.lindy@yahoo.com>
2022-06-01 17:37:08 -0500

    updated resume section styling

--- portfolio/src/Components/Resume.js
+++ portfolio/src/Components/Resume.js
@@ -1,13 +1,14 @@
 import React from "react";
+import driveLogo from "../Images/google-drive-logo.png"
 
 export default function Resume() {
     return (
         <section id="resumeCard" className="card">
-            <h2 id="Resume" className="titles">Resume</h2>
-            <section className="content">
-                <h4 className="space"><a href="https://drive.google.com/file/d/1b-LsVq1alS6q4p9Jo7JQcQMyMVoedzOl/view?usp=sharing">View Resume</a></h4>
-                <h4>Developer Proficiencies</h4>
-                <p>Git | JavaScript | Node.js | React.js | CSS | HTML5 | Express.js | MERN Stack | MongoDB | Mongoose | MySQL | Sequelize</p>
+            <h2 id="Resume" className="card-title text-center lh-lg">RESUME</h2>
+            <section>
+                <h5 className="text-center" ><img style={{ maxWidth: "2.2rem" }} src={driveLogo} alt="google drive logo"></img><a style={{ textDecoration: 'none' }} href="https://drive.google.com/file/d/1b-LsVq1alS6q4p9Jo7JQcQMyMVoedzOl/view?usp=sharing">view pdf</a></h5>
+                {/* <h4>Developer Proficiencies</h4> */}
+                <p className="text-center"> Git | JavaScript | Node.js | React.js | CSS | HTML5 | Express.js | MERN Stack | MongoDB | Mongoose | MySQL | Sequelize</p>
             </section>
         </section>
     )
