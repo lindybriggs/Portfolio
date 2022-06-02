2771c584786705a5332b8c4690bf381c5a7a8fdd
Lindy Briggs <briggs.lindy@yahoo.com>
2022-06-02 17:06:47 -0500

    about section updated

--- portfolio/src/Components/About.js
+++ portfolio/src/Components/About.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import headShot from "../Images/head-shot.jpeg"
+
+const imageStyle = {
+    width: "10rem",
+    borderRadius: "50%"
+}
+
+const paragraphStyle = {
+    margin: "1.3rem",
+    textAlign: "justify",
+    textJustify: "inter-word"
+}
 
 export default function About() {
     return (
-        <section className="card">
-            <h2 id="About" className="titles">About Me</h2>
-            <p className="content">
-                Full-stack web developer leveraging analytical marketing and media planning background to improve web efficiency and system intuitiveness. Certified in full stack development from Northwestern Bootcamps. Known as an organized, detail-oriented team player with a passion for problem solving.
-            </p>
+        <section id="resumeCard" className="card">
+            <h2 id="About" className="card-title text-center lh-lg">ABOUT</h2>
+            <div className='row justify-content-center py-3 card-body'>
+                <img className='col-sm-9 col-md-3' style={imageStyle} src={headShot} alt="developer head shot"></img>
+                <p className="col-sm-9 col-md-7 text-justify" style={paragraphStyle}>
+                    Full-stack web developer leveraging analytical marketing and media planning background to improve web efficiency and system intuitiveness. Certified in full stack development from Northwestern Bootcamps. Known as an organized, detail-oriented team player with a passion for problem solving.
+                </p>
+            </div>
         </section>
     )
 };
--- portfolio/src/Components/Nav.js
+++ portfolio/src/Components/Nav.js
@@ -17,13 +17,13 @@ export default function Nav({ currentPage, handlePageChange }) {
                     <a href="#Home" onClick={() => handlePageChange('Home')} style={currentPage === 'Home' ? active : inactive}>Home</a>
                 </li>
                 <li>
-                    <a href="#About" onClick={() => handlePageChange('About')} style={currentPage === 'About' ? active : inactive}>About Me</a>
+                    <a href="#About" onClick={() => handlePageChange('About')} style={currentPage === 'About' ? active : inactive}>About</a>
                 </li>
                 <li>
-                    <a href="#Work" onClick={() => handlePageChange('Work')} style={currentPage === 'Work' ? active : inactive}>My Work</a>
+                    <a href="#Work" onClick={() => handlePageChange('Work')} style={currentPage === 'Work' ? active : inactive}>Work</a>
                 </li>
                 <li>
-                    <a href="#Contact" onClick={() => handlePageChange('Contact')} style={currentPage === 'Contact' ? active : inactive}>Contact Me</a>
+                    <a href="#Contact" onClick={() => handlePageChange('Contact')} style={currentPage === 'Contact' ? active : inactive}>Contact</a>
                 </li>
                 <li>
                     <a href="#Resume" onClick={() => handlePageChange('Resume')} style={currentPage === 'Resume' ? active : inactive}>Resume</a>
