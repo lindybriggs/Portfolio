47b3c10a361a69466453d16a7988ab37157702e3
Lindy Briggs <briggs.lindy@yahoo.com>
2022-06-03 09:47:18 -0500

    updated work cards

--- portfolio/src/Components/Work.js
+++ portfolio/src/Components/Work.js
@@ -6,21 +6,47 @@ import schedulerPhoto from '../Images/scheduler.png'
 export default function Work() {
     return (
         <section className="card">
-            <h2 id="Work" className="titles">My Work</h2>
-            <section className="content">
-                <div className="try">
-                    <a href="https://therecipeforsuccess.herokuapp.com/" ><img className="bigWork" src={recipePhoto} alt="full stack group project - the recipe for success" /></a>
-                    <h3>The Recipe for Success: <a href="https://github.com/mcjbyday/therecipeforsuccess">GitHub</a> </h3>
+            <div className='card-body'>
+            <h2 id="Work" className="card-title text-center lh-lg">WORK</h2>
+
+            <section className="row justify-content-center">
+
+                <div className="col-sm-10 col-md-4">
+                    <div className="card text-center " >
+                        <img src={recipePhoto} className="card-img-top" alt="recipe for success deployed homepage" />
+                        <div className="card-body align-content-center">
+                            <h5 className="card-title text-center">The Recipe for Success</h5>
+                            <a href="https://github.com/mcjbyday/therecipeforsuccess" className="btn btn-secondary m-1">GitHub</a>
+                            <a href="https://therecipeforsuccess.herokuapp.com/" className="btn btn-secondary m-1">Deployed App</a>
+                        </div>
+                    </div>
                 </div>
-                <div className="other1">
-                    <a href="https://limitless-wave-23866.herokuapp.com/"><img className="otherWork1" src={noteTakerPhoto} alt="note taker app" /></a>
-                    <h3>Note Taker: <a href="https://github.com/lindybriggs/Note-Taker">GitHub</a> </h3>
+
+                <div className="col-sm-10 col-md-4">
+                    <div className="card text-center" >
+                        <img src={noteTakerPhoto} className="card-img-top" alt="note taker homepage" />
+                        <div className="card-body align-content-center">
+                            <h5 className="card-title text-center">Note Taker</h5>
+                            <a href="https://github.com/lindybriggs/Note-Taker" className="btn btn-secondary m-1">GitHub</a>
+                            <a href="https://limitless-wave-23866.herokuapp.com/" className="btn btn-secondary m-1">Deployed App</a>
+                        </div>
+                    </div>
                 </div>
-                <div className="other2">
-                    <a href="https://lindybriggs.github.io/hw5-scheduler/"><img className="otherWork2" src={schedulerPhoto} alt="another example of my work" /></a>
-                    <h3>Scheduler: <a href="https://github.com/lindybriggs/hw5-scheduler">GitHub</a></h3>
+
+                <div className="col-sm-10 col-md-4">
+                    <div className="card text-center" >
+                        <img src={schedulerPhoto} className="card-img-top" alt="work day scheduler home page" />
+                        <div className="card-body align-content-center">
+                            <h5 className="card-title text-center">Work Day Scheduler</h5>
+                            <a href="https://github.com/lindybriggs/hw5-scheduler" className="btn btn-secondary m-1">GitHub</a>
+                            <a href="https://lindybriggs.github.io/hw5-scheduler/" className="btn btn-secondary m-1">Deployed App</a>
+                        </div>
+                    </div>
                 </div>
-            </section>
+
         </section>
+
+        </div>
+        </section >
     )
 };
